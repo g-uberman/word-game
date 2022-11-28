e6536d2b5395d31f426738914c522c6c89e8d052
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-28 14:23:48 +0100

    footer styled

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,5 +1,11 @@
+import { StyledFooter } from "./Styles/Footer.styles";
+
 export const Footer = () => {
-    return (
-    <>Footer</>
-    )
-}
+  return (
+    <StyledFooter>
+      <a href="https://github.com/g-uberman" target="blank">
+        g-uberman 2022
+      </a>
+    </StyledFooter>
+  );
+};
---
+++ src/Components/Styles/Footer.styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const StyledFooter = styled.summary`
+    text-align: center;
+    font-size: 0.8em;
+    margin-bottom: 0.3em;
+`

6199daee1a4fc15860bfecd9bbb64a3207e05e37
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-29 19:11:46 +0100

    game screen

--- src/Components/GameScreen.tsx
+++ src/Components/GameScreen.tsx
@@ -1,5 +1,24 @@
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { ScreenWrapper } from "./Styles/Shared.styles";
+import { Button, TextField } from "@mui/material";
+import { Context } from "./../ContextProvider";
+
 export const GameScreen = () => {
-    return (
-        <>Game Screen</>
-        )
-}
+  const { username } = useContext(Context);
+  const navigate = useNavigate();
+
+  const handleCheck = () => {
+    navigate("/result");
+  };
+
+  return (
+    <>
+      <h1>Select animals</h1>
+      <ScreenWrapper>{username}</ScreenWrapper>
+      <Button variant="contained" sx={{ margin: "24.65px" }} onClick={handleCheck}>
+        Check answers
+      </Button>
+    </>
+  );
+};
--- src/Components/Styles/App.styles.tsx
+++ src/Components/Styles/App.styles.tsx
@@ -12,6 +12,7 @@ export const MainSection = styled.div`
     width: 100%;
     flex-grow: 1;
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
 `

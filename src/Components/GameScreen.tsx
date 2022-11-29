ec050e4f0738eeb81a87bd15d5b2ca64ed19e4d3
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-29 19:19:28 +0100

    game screen layout and navigation

--- src/Components/GameScreen.tsx
+++ src/Components/GameScreen.tsx
@@ -1,14 +1,20 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ScreenWrapper } from "./Styles/Shared.styles";
 import { Button, TextField } from "@mui/material";
 import { Context } from "./../ContextProvider";
 
 export const GameScreen = () => {
-  const { username } = useContext(Context);
+  const { username, setPoints } = useContext(Context);
+  const [gameover, setGameover] = useState(false);
   const navigate = useNavigate();
 
   const handleCheck = () => {
+    setGameover(true);
+    setPoints(0);
+  };
+
+  const handleFinish = () => {
     navigate("/result");
   };
 
@@ -16,9 +22,22 @@ export const GameScreen = () => {
     <>
       <h1>Select animals</h1>
       <ScreenWrapper>{username}</ScreenWrapper>
-      <Button variant="contained" sx={{ margin: "24.65px" }} onClick={handleCheck}>
+      {!gameover &&
+      (<Button
+        variant="contained"
+        sx={{ margin: "24.65px" }}
+        onClick={handleCheck}
+      >
         Check answers
-      </Button>
+      </Button>)}
+      {gameover &&
+      (<Button
+        variant="contained"
+        sx={{ margin: "24.65px" }}
+        onClick={handleFinish}
+      >
+        Finish game
+      </Button>)}
     </>
   );
 };

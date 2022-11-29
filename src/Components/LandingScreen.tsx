1f321e6b9ee730743b21b817e0ef247986b6d49c
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-29 18:53:21 +0100

    username handling

--- src/Components/LandingScreen.tsx
+++ src/Components/LandingScreen.tsx
@@ -1,16 +1,34 @@
+import { useContext } from "react";
 import { ScreenWrapper } from "./Styles/Shared.styles";
 import { Button, TextField } from "@mui/material";
+import { Context } from "./../ContextProvider";
 
 export const LandingScreen = () => {
+  const { username, setUsername } = useContext(Context);
 
-    
+  const handleUsername = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+      ) => {
+        setUsername(e.target.value);
+      };
+
+  const handlePlay = () => {};
+
   return (
     <ScreenWrapper>
       <main style={{ paddingBottom: "5em" }}>
         <h1>WordCloud Game</h1>
-        <TextField type="text" placeholder="Enter name" />
+        <TextField
+          type="text"
+          placeholder="Enter name"
+          onChange={(e) => handleUsername(e)}
+        />
         <br />
-        <Button variant="contained" sx={{ margin: "2em" }}>
+        <Button
+          variant="contained"
+          sx={{ margin: "2em" }}
+          onChange={handlePlay}
+        >
           Play
         </Button>
       </main>

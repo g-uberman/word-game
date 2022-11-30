863e8ddd15bf4ff0a0d84d313997fa405c1b61ff
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-30 12:04:30 +0100

    data tsx added

---
+++ src/data.tsx
@@ -0,0 +1,59 @@
+interface Questions {
+  question: string;
+  all_words: string[];
+  good_words: string[];
+}
+
+export const animals: Questions = {
+  question: "select animals",
+  all_words: [
+    "hole",
+    "sofa",
+    "pear",
+    "tiger",
+    "oatmeal",
+    "square",
+    "nut",
+    "cub",
+    "shirt",
+    "tub",
+    "passenger",
+    "cow",
+  ],
+  good_words: ["tiger", "cow"],
+};
+
+export const colors: Questions = {
+  question: "select colors",
+  all_words: [
+    "jeans",
+    "existence",
+    "ink",
+    "red",
+    "blue",
+    "yellow",
+    "laugh",
+    "behavior",
+    "expansion",
+    "white",
+    "black",
+    "cakes",
+  ],
+  good_words: ["red", "blue", "yellow", "white", "black"],
+};
+
+export const vehicles: Questions = {
+  question: "select vehicles",
+  all_words: [
+    "belief",
+    "wire",
+    "car",
+    "bus",
+    "star",
+    "river",
+    "hat",
+    "skirt",
+    "train",
+  ],
+  good_words: ["car", "bus", "train"],
+};

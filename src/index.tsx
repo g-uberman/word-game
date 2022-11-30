d920a5637d40176aceeb7f79f056968fd1a93a9c
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-30 12:49:45 +0100

    added GlobalStyle

--- src/index.tsx
+++ src/index.tsx
@@ -5,12 +5,15 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ContextProvider } from "./ContextProvider";
+import { GlobalStyle } from "./Components/Styles/GlobalStyle";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <ContextProvider>
       <Router>
         <App />

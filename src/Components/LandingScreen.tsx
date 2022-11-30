be1cd6ac58e31e71f99c0c5b085fc394ad5d302a
Grzegorz Uberman <ubermangrzegorz@gmail.com>
2022-11-30 19:29:12 +0100

    username input handling enter

--- src/Components/LandingScreen.tsx
+++ src/Components/LandingScreen.tsx
@@ -9,31 +9,34 @@ export const LandingScreen = () => {
   const navigate = useNavigate();
 
   const handleUsername = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-      ) => {
-        setUsername(e.target.value);
-      };
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setUsername(e.target.value);
+  };
 
   const handlePlay = () => {
     navigate("/game");
   };
 
-  return (
-      <ClearWrapper>
-        <h1>WordCloud Game</h1>
-        <TextField
-          type="text"
-          placeholder="Enter name"
-          onChange={(e) => handleUsername(e)}
-        />
-        <br />
-        <Button
-          variant="contained"
-          onClick={handlePlay}
-        >
-          Play
-        </Button>
-      </ClearWrapper>
+  const handleEnter = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    if (e.key === "Enter") {
+      handlePlay();
+    }
+  };
 
+  return (
+    <ClearWrapper>
+      <h1>WordCloud Game</h1>
+      <TextField
+        type="text"
+        placeholder="Enter name"
+        onChange={(e) => handleUsername(e)}
+        onKeyDown={(e) => handleEnter(e)}
+      />
+      <br />
+      <Button variant="contained" onClick={handlePlay}>
+        Play
+      </Button>
+    </ClearWrapper>
   );
 };
